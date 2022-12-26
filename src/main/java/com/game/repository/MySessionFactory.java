52a036b1b6b706fc624968a233e598f5a514a1a8
sfill70 <sergfill70@yandex.ru>
2022-12-26 20:57:30 +0300

    edit 1

--- src/main/java/com/game/repository/MySessionFactory.java
+++ src/main/java/com/game/repository/MySessionFactory.java
@@ -13,13 +13,13 @@ public class MySessionFactory {
     private static MySessionFactory instance;
     private final SessionFactory sessionFactory;
     private MySessionFactory() {
-
-/*        Properties properties = new Properties();
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+/*      Properties properties = new Properties();
         properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
         properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "q1w2e3r4ty");
+        properties.put(Environment.PASS, "password");
         properties.put(Environment.HBM2DDL_AUTO, "update");
 
         sessionFactory = new Configuration().
@@ -28,8 +28,6 @@ public class MySessionFactory {
                 addAnnotatedClass(Profession.class).
                 addAnnotatedClass(Race.class).
                 buildSessionFactory();*/
-
-       sessionFactory = new Configuration().configure().buildSessionFactory();
     }
     public static SessionFactory getSessionFactory() {
         if (instance == null) {
